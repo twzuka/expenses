265cfce25eb2e5e31ba858586ad252042e05d754
twzuka-pc <twzuka@gmail.com>
2026-04-23 22:58:11 +0300

    Создал функции add и add_category

--- expenses.py
+++ expenses.py
@@ -1,11 +1,49 @@
-import sys
+import sys, json
 
 commands = {"add", "add-category", "list", "total"}
 
-if len(sys.argv) >= 5 or (sys.argv[1] not in commands):
-    print("Неизвестная команда!")
-    print(sys.argv)
-    sys.exit(1)
+FILE_NAME = "data.json"
 
-if sys.argv[1] == "add-category":
+
+def start():
+    try:
+        with open(FILE_NAME, "r") as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        data = {}
+    return data
+
+
+def save(data):
+    with open(FILE_NAME, "w") as file:
+        json.dump(data, file, indent=4)
+
+
+def add_category(cat):
+    try:
+        with open(FILE_NAME, "r") as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        data = {}
+
+    if cat not in data:
+        data[cat] = []
+
+    with open(FILE_NAME, "w") as file:
+        json.dump(data, file, indent=4)
+
+
+def add(price, cat, name):
+    try:
+        int_price = int(price)
+    except ValueError:
+        print("Ошибка: введено не число!")
+        return
+    data = start()
+    if cat in data:
+        data[cat].append([int_price, name])
+        save(data)
+    else:
+        print("Ошибка: такой категории не существует!")
+        return
     
