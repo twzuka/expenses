7bfd934853552da78087bb4677b1e7acd73d7567
twzuka-pc <twzuka@gmail.com>
2026-04-25 12:41:00 +0300

    Реализовал логику программы, создал функции "list", "total", "help".

--- expenses.py
+++ expenses.py
@@ -1,13 +1,13 @@
 import sys, json
 
-commands = {"add", "add-category", "list", "total"}
+commands = {"add", "add-category", "list", "total", "help"}
 
 FILE_NAME = "data.json"
 
 
 def start():
     try:
-        with open(FILE_NAME, "r") as file:
+        with open(FILE_NAME, "r", encoding="utf-8") as file:
             data = json.load(file)
     except FileNotFoundError:
         data = {}
@@ -15,22 +15,27 @@ def start():
 
 
 def save(data):
-    with open(FILE_NAME, "w") as file:
-        json.dump(data, file, indent=4)
+    with open(FILE_NAME, "w", encoding="utf-8") as file:
+        json.dump(data, file, indent=4, ensure_ascii=False)
+
+
+def help():
+    print("--- СПИСОК ВСЕХ КОМАНД ---")
+    print(
+        "1. Добавить расход: «python expenses.py add <стоимость> <категория> <название>»"
+    )
+    print("2. Добавить категорию: «python expenses.py add-category <категория>»")
+    print("3. Показать все расходы: «python expenses.py list [категория]»")
+    print("4. Показать сумму расходов: «python expenses.py total  [категория]»")
 
 
 def add_category(cat):
-    try:
-        with open(FILE_NAME, "r") as file:
-            data = json.load(file)
-    except FileNotFoundError:
-        data = {}
-
+    data = start()
     if cat not in data:
         data[cat] = []
-
-    with open(FILE_NAME, "w") as file:
-        json.dump(data, file, indent=4)
+        save(data)
+    else:
+        print("Ошибка: категория уже существует!")
 
 
 def add(price, cat, name):
@@ -46,4 +51,56 @@ def add(price, cat, name):
     else:
         print("Ошибка: такой категории не существует!")
         return
-    
+
+
+def list_cat(cat_arg):
+    data = start()
+
+    if not cat_arg or not cat_arg.strip():
+        print(f"--- ПОЛНЫЙ СПИСОК РАСХОДОВ ---")
+        for cat, exp in data.items():
+            print(f"Категория: {cat}")
+            for item in exp:
+                print(f"-- {item[1]}: {item[0]} руб.")
+    elif cat_arg in data:
+        print(f"--- РАСХОДЫ ПО КАТЕГОРИИ: {cat_arg} ---")
+        for item in data[cat_arg]:
+            print(f"-- {item[1]}: {item[0]} руб.")
+    else:
+        print(f"Ошибка: категории '{cat_arg}' не существует.")
+
+
+def total_exp(cat_arg):
+    data = start()
+
+    if not cat_arg or not cat_arg.strip():
+        print("--- ПОЛНАЯ СУММА РАСХОДОВ ---")
+        for cat, exp in data.items():
+            total = 0
+            for item in exp:
+                total += int(item[0])
+            print(f"Категория {cat}: {total} руб.")
+    elif cat_arg in data:
+        total = 0
+        for item in data[cat_arg]:
+            total += int(item[0])
+        print(f"--- Всего по категории {cat_arg}: {total} руб. ---")
+
+
+args = sys.argv
+if len(args) > 1:
+    if args[1] in commands:
+        if args[1] == "add" and len(sys.argv) == 5:
+            add(args[2], args[3], args[4])
+        elif args[1] == "add-category" and len(sys.argv) == 3:
+            add_category(args[2])
+        elif args[1] == "list":
+            list_cat(args[2] if len(sys.argv) == 3 else None)
+        elif args[1] == "total":
+            total_exp(args[2] if len(sys.argv) == 3 else None)
+        elif args[1] == "help" and len(sys.argv) == 2:
+            help()
+        else:
+            print("Ошибка: Неизвестная команда!")
+else:
+    help()
